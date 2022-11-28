84abb76e51793e58ca961a7f379960aeb47c590f
brad1024 <harry1024harry1024@gmail.com>
2022-11-29 01:47:09 +0800

    debug

--- mcts.h
+++ mcts.h
@@ -37,14 +37,14 @@ public:
             totalVisit += legalNodes[i]->visitCount;
         }
         for(int i=0; i<legalNodes.size(); i++){
-            //std::cout << value << " ";
+            std::cout << value << " ";
             value = -legalNodes[i]->value/(legalNodes[i]->visitCount+EPSILON) + sqrt(2*log(totalVisit)/(legalNodes[i]->visitCount+EPSILON));
             if(value>=maxValue){
                 maxValue = value;
                 index = i;
             }
         }
-        std::cout << "index: " << index << std::endl;
+        std::cout << "\nindex: " << index << std::endl;
         return legalNodes[index];
     } 
     void Expand(board::piece_type who){
