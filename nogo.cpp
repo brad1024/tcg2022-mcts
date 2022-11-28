f2b772636ba75612d5b97ea50b95e36ed76a9250
brad1024 <harry1024harry1024@gmail.com>
2022-11-28 16:50:02 +0800

    debug

--- nogo.cpp
+++ nogo.cpp
@@ -69,7 +69,7 @@ int main(int argc, const char* argv[]) {
 		if (stats.is_finished()) stats.summary();
 	}
 
-	player black("name=black " + black_args + " role=black");
+	MCTS_Player black("name=black " + black_args + " role=black");
 	MCTS_Player white("name=white " + white_args + " role=white");
 	printf("game start");
 	if (!shell) { // launch standard local games
