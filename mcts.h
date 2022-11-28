136febc863a05c61ef068863851ad93203e2d9ad
brad1024 <harry1024harry1024@gmail.com>
2022-11-28 16:37:26 +0800

    debug

--- mcts.h
+++ mcts.h
@@ -45,7 +45,6 @@ public:
         
         for (const action::place& move : space) {
 			board after = state;
-            std::cout << move.apply(after) << std::endl;
 			if (move.apply(after) == board::legal){
                 legalNodes.push_back(new Node(after));
                 legalMoves.push_back(move);
